84a1e613e530703daaa17a91666ed87e990c557a
BePsvPT <freedom@csie.io>
2016-01-02 20:33:33 +0800

    Update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,13 +4,15 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 
-$router->get('/', ['uses' => 'HomeController@home']);
-
-$router->group(['domain' => 'ecourse.ccu.plus'], function (Router $router) {
+$router->group(['middleware' => ['web']], function (Router $router) {
     $router->get('/', ['uses' => 'HomeController@home']);
-});
 
-$router->group(['domain' => 'sso.ccu.plus', 'namespace' => 'Sso', 'as' => 'sso.'], function (Router $router) {
-    $router->post('sign-in', ['as' => 'signIn', 'uses' => 'AuthController@signIn']);
-    $router->get('sign-out', ['as' => 'signOut', 'uses' => 'AuthController@signOut']);
+    $router->group(['domain' => 'ecourse.ccu.plus'], function (Router $router) {
+        $router->get('/', ['uses' => 'HomeController@home']);
+    });
+
+    $router->group(['domain' => 'sso.ccu.plus', 'namespace' => 'Sso', 'as' => 'sso.'], function (Router $router) {
+        $router->post('sign-in', ['as' => 'signIn', 'uses' => 'AuthController@signIn']);
+        $router->get('sign-out', ['as' => 'signOut', 'uses' => 'AuthController@signOut']);
+    });
 });
