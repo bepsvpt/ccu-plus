6722315585c5280cecd20f09a3581d8da16642cb
BePsvPT <freedom@csie.io>
2016-01-02 23:03:19 +0800

    Move User model to Ccu directory

--- app/Ccu/User.php
+++ app/Ccu/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Ccu;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,7 +11,7 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Ccu\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
