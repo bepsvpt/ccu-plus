5969133f658e0e94a5dcde65192a52474137077e
BePsvPT <freedom@csie.io>
2016-02-02 01:05:37 +0800

    Fix style

--- resources/assets/js/components/recaptcha.vue
+++ resources/assets/js/components/recaptcha.vue
@@ -7,7 +7,9 @@
         data() {
             return {
                 recaptcha: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                    var r = Math.random() * 16 | 0,
+                        v = c == 'x' ? r : (r & 0x3 | 0x8);
+
                     return v.toString(16);
                 })
             };
