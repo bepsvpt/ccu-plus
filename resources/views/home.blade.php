20bf4995565c5d1bf4c6070a1897b25c409fe4c5
BePsvPT <freedom@csie.io>
2016-02-03 23:37:00 +0800

    Add logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <meta property="og:title" content="CCU PLUS | 全新生活 由此領航">
+        <meta property="og:title" content="CCU Plus | 全新生活 由此領航">
         <meta property="og:type" content="website">
         <meta property="og:url" content="{{ route('home') }}">
+        <meta property="og:image" content="{{ asset('assets/images/logo.png') }}">
         <meta property="og:locale" content="zh_TW">
-        <title>CCU PLUS | 全新生活 由此領航</title>
+        <title>CCU Plus | 全新生活 由此領航</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -18,7 +19,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js" defer></script>
         <script src="https://www.google.com/recaptcha/api.js?onload=grecaptchaLoaded&render=explicit" defer></script>
-        <script src="/assets/js/arrive.min.js" defer></script>
+        <script src="{{ asset('assets/js/arrive.min.js') }}" defer></script>
         <script src="{{ $js }}" defer></script>
     </body>
 </html>
