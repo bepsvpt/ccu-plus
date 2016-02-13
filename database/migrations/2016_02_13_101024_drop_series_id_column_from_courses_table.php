daae8a25bcfbdf471c2c6c2fcfa864e8ec1fab30
BePsvPT <freedom@csie.io>
2016-02-13 12:46:58 +0800

    Prepare for drop series id column from courses table

---
+++ database/migrations/2016_02_13_101024_drop_series_id_column_from_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropSeriesIdColumnFromCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropColumn('series_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->unsignedSmallInteger('series_id')->default(0)->after('name');
+
+            $table->index('series_id');
+        });
+    }
+}
