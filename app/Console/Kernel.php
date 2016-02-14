777929d1a7fb7e574f7d539ce5d02b356c371364
BePsvPT <freedom@csie.io>
2016-02-14 14:11:23 +0800

    Backup transform code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
         Commands\Deploy::class,
         Commands\DownloadCourseArchive::class,
         Commands\ImportCourse::class,
+        Commands\TransformOldData::class,
     ];
 
     /**
