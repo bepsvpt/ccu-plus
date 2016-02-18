0ade93c63fa6d13ff72fef3f897fb4f142cb93fc
BePsvPT <freedom@csie.io>
2016-02-18 16:01:39 +0800

    Increment version

--- app/Ccu/Core/Entity.php
+++ app/Ccu/Core/Entity.php
@@ -15,7 +15,7 @@ class Entity extends Eloquent
     /**
      * CCU Plus Version.
      */
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     /**
      * The number of models to return for pagination.
