0967e57a773988b707a74d753666fa98ca405ae4
BePsvPT <freedom@csie.io>
2016-10-11 11:30:07 +0800

    Build

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
     <title>CCU Plus | 全新生活 由此領航</title>
     <link rel="stylesheet" href="{{ asset('assets/css/bulma.min.css') }}" integrity="sha384-hE6QNVYMjfoCI4CUPK++G5c4ZuS3Dk/nDsE+ytMA06IU4e3Qo7PQSPHDnmsecrqd">
     <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1">
+    <link rel="stylesheet" href="{{ _asset('main.css') }}" integrity="sha384-uyxfdspB8FOR/XkA8KulVXt/u9i1mjbh99GntJTwiRPjdEEI8OGGAEu+WfuHJgOr">
   </head>
   <body>
     <div id="ccu-plus">
@@ -24,7 +25,7 @@
     </div>
 
     <script src="https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit" defer></script>
-    <script src="{{ _asset('vendor.js') }}" integrity="sha384-BIyeTAPUiNVJKk3ByNla8KTOfxeI9pvIQIYj/AQX4bNWj0BQdqInvSE4f0v3K7Sr" defer></script>
-    <script src="{{ _asset('main.js') }}" integrity="sha384-CIcXA6yTvEunyVmLUVKFPaUGVowQFQwZwV7w3UFrwCw1I7Eb1OcVOqnAAUxo1YcV" defer></script>
+    <script src="{{ _asset('vendor.js') }}" integrity="sha384-DK3H0bRdXLe1X4MwudeAd742Ma2ZIEaswcv5G+15LETjm3xYeHmb4MY40vYYLhPt" defer></script>
+    <script src="{{ _asset('main.js') }}" integrity="sha384-jSuEjjLmTN/zbaME/U0DH3lt2gYnU9KuHhdsYVS7hyPW2OBwmA7iShOufutIOUoy" defer></script>
   </body>
 </html>
